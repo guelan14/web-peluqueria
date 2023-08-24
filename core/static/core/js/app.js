42849bbbfba84bcbee041c868028507f476b36ea
Miguel Angel Neumann <miguelangelneumann@gmail.com>
2023-08-24 16:13:28 -0300

    arreglado primera pagina

--- core/static/core/js/app.js
+++ core/static/core/js/app.js
@@ -19,9 +19,9 @@ function realizarDesplazamiento() {
   var divLeft = document.getElementById('div-left');
   var divRight = document.getElementById('div-right');
 
-  // Aplicar el desplazamiento a ambos divs
+  //Aplicar el desplazamiento a ambos divs
   divLeft.style.opacity = '1';
-  divLeft.style.transform = 'translateX(0)';
+    divLeft.style.transform = 'translateX(0)';
 
   divRight.style.opacity = '1';
   divRight.style.transform = 'translateX(0)';
